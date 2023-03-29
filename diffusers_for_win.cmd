1f08c4ec467d2aa9affaf4d1f5c06d5a4358919b
pyxudev <92786015+pyxudev@users.noreply.github.com>
2023-03-29 16:27:20 +0900

    Update diffusers_for_win.cmd

--- diffusers_for_win.cmd
+++ diffusers_for_win.cmd
@@ -1,19 +1,29 @@
-echo "Startingdiffusers setup"
-cd [YOUR_APP_PATH]
-
-echo "Activating venv environment"
-python -m venv [YOUR_PROJEC_NAME]
-cd [YOUR_PROJECT_NAME]
-call .\Scripts\activate
-mkdir [YOUR_APP_NAME]
-
-echo "Installing packages"
-pip install --upgrade diffusers[torch]
-pip install --upgrade diffusers[flax]
-pip install transformers
-pip install diffusion==0.8.0
-
 @ECHO OFF
 :INPUT_START
+echo Startingdiffusers setup
+
+SET INPUTSTR=
+SET /P INPUTSTR="Enter your project path:"
+cd %INPUTSTR%
+
+echo Activating venv environment
+SET INPUTSTR=
+SET /P INPUTSTR="Enter your project name:
+
+python -m venv %INPUTSTR%
+cd %INPUTSTR%
+
+SET INPUTSTR=
+SET /P INPUTSTR="Enter your app name:"
+mkdir %INPUTSTR%
+
+echo Installing packages
+call .\Scripts\activate
+pip install --upgrade diffusers[torch]
+pip install diffusers==0.4.0
+pip install --upgrade diffusers[flax]
+pip install transformers
+pip install diffusers==0.8.0
+
 SET INPUTSTR=
 SET /P INPUTSTR="Done"
